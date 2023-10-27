6e0c0e175c0ee68c971578956135c5deaa8156bb
@OneJuicyDoor <shephardcam@gmail.com>
2023-10-27 06:49:46 -0300

    add functionality to admin page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "jungle", password: "book"
   def show
+
+    @total_stock = Product.sum(:quantity)
+
+    @number_of_items = Product.count
   end
+  
 end
